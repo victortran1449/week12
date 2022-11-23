b3b2d4780280448b4583404893a28ed323d051ad
Victor Tran <victortran1449@gmail.com>
2022-11-23 13:21:28 -0800

    more add event listener practice with if else loops

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,22 @@
 const clickMeButton = document.querySelector(".click-me");
 // console.log(clickMeButton)
-
 const clickHandler = () => {
   alert("You clicked me!");
   // clickMeButton.removeEventListener("click", clickHandler)
 };
-
 clickMeButton.addEventListener("click", clickHandler, { once: true });
 
 const changeBGPink = () => {
   // document.body.style.backgroundColor = "pink"
-  document.body.classList.add("pink");
+  document.body.classList.toggle("pink");
 };
-
 clickMeButton.addEventListener("click", changeBGPink);
+
+const changeText = () => {
+  if (clickMeButton.textContent === "Click Me!") {
+    clickMeButton.textContent = "clicked!";
+  } else if (clickMeButton.textContent === "clicked!") {
+    clickMeButton.textContent = "Click Me!";
+  }
+};
+clickMeButton.addEventListener("click", changeText);
