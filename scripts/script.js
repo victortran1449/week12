e4653d1be4fec63c7a7af77d84a2355a16d58432
Victor Tran <victortran1449@gmail.com>
2022-11-23 12:57:26 -0800

    add event listener practice

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,16 @@
-const clickMeButton = document.querySelector(".click-me")
+const clickMeButton = document.querySelector(".click-me");
 // console.log(clickMeButton)
 
 const clickHandler = () => {
-    alert("You clicked me!")
-    // clickMeButton.removeEventListener("click", clickHandler)
-}
+  alert("You clicked me!");
+  // clickMeButton.removeEventListener("click", clickHandler)
+};
 
-clickMeButton.addEventListener("click", clickHandler, {once:true})
+clickMeButton.addEventListener("click", clickHandler, { once: true });
 
+const changeBGPink = () => {
+  // document.body.style.backgroundColor = "pink"
+  document.body.classList.add("pink");
+};
+
+clickMeButton.addEventListener("click", changeBGPink);
