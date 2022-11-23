663e5cf5c45e254a07e591fba2f33c910df7f0ff
Victor Tran <victortran1449@gmail.com>
2022-11-23 14:18:44 -0800

    event listener practice with event object

--- scripts/script.js
+++ scripts/script.js
@@ -21,8 +21,24 @@ const changeText = () => {
 };
 clickMeButton.addEventListener("click", changeText);
 
-const updateImage = () => {
-    const image = document.querySelector("#shoppingCart")
-    image.setAttribute("src", "images/shoppingcart.png")
-}
-clickMeButton.addEventListener("click", updateImage)
+const updateImage = (e) => {
+  // console.log(e.target.textContent)
+  const image = document.querySelector("#shoppingCart");
+  image.setAttribute("src", "images/shoppingcart.png");
+  image.setAttribute("width", "100");
+  image.setAttribute("height", "100");
+};
+clickMeButton.addEventListener("click", updateImage);
+
+const buttonContainer = document.querySelector(".button-container");
+const changeBGGreen = (event) => {
+  if (event.target.tagName === "BUTTON") {
+    event.target.classList.add("green");
+  }
+};
+buttonContainer.addEventListener("mouseover", changeBGGreen);
+
+const changeTextColor = (event) => {
+  event.target.style.color = event.target.textContent;
+};
+buttonContainer.addEventListener("click", changeTextColor);
